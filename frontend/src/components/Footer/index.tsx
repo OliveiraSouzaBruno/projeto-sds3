de8c08570cf4deca07d1def19987e09f4c94728f
OliveiraSouzaBruno <oliveira_bruno2018@hotmail.com>
2021-05-04 00:05:14 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/OliveiraSouzaBruno" target="_blank" rel="noreferrer">Bruno Oliveira</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://www.instagram.com/brunos_ol/" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
